4c8f3cf637049bf8c9dc629e47a6876a0adbd422
Jahmeeu-Cloud <jamiu.o.bakre@gmail.com>
2025-04-16 19:59:40 +0100

    updated code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello Horla! You are getting there, working as a Professional Cloud DevOps Engineer');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
